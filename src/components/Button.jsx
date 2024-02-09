a6b98dd484936b2d3d5d51b7a619d6a99734aa8d
Razvan <razvule@gmail.com>
2024-02-09 15:02:43 +0200

    Creat alt buton
    Am folosit aceasi componenta si am creat 2 butoane

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Button.css'
 
-function apasaButon() {
-  return alert('Ai apasat cumva pe buton?')
-}
 
-export default function Button() {
+
+export default function Button({mesaj, text}) {
+  function apasaButon() {
+    return alert(mesaj)
+  }
   return (
-    <button onClick={apasaButon}>Apasa-l</button>
+    <button onClick={apasaButon}>{text}</button>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import Main from './components/Main';
 import Button from './components/Button';
 
+const mesaj1 = 'Ai apasat cumva pe buton?'
+const textButon1 = 'Apasa-l'
+
+const mesaj2 = 'Cum ai ajuns aici?'
+const textButon2 = 'Text'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Main />
-    <Button />
+    <Button mesaj={mesaj1} text={textButon1} />
+    <Button mesaj={mesaj2} text={textButon2} />
   </React.StrictMode>
 )
