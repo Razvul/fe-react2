79bd91b5df60161395db3fb8ff5e38ba30ba65be
Razvan <razvule@gmail.com>
2024-02-11 21:20:37 +0200

    Am folosit altfel componenta

--- src/components/Button2.jsx
+++ src/components/Button2.jsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react'
 import './Button.css'
 import Button from './Button'
 
-export default function Button2({ textButon2, mesaj2, apasaButon, color }) {
-    const [textButon, setTextButon] = useState('Nu atinge!')
+export default function Button2({ textButon2, mesaj2, color }) {
+    const [textButon, setTextButon] = useState(textButon2)
     return (
-        <Button onClick={() => setTextButon(textButon2)}
-            // text={textButon2} mesaj={mesaj2}  color={color}
-        >{textButon}
-        </Button>
+        <Button text={textButon} mesaj={mesaj2} color={color}
+            handleClick={()=>setTextButon(textButon2)}
+        />
     )
 }
+
+// 
