6d4678376a323a112ba881fc40e9d79424985949
Razvan <razvule@gmail.com>
2024-02-17 18:49:02 +0200

    Si am facut sa mearga butonul

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Button.css'
 
-export default function Button({ mesaj, text, handleClick, color }) {
+export default function Button({ handleClick, text, color }) {
   return (
-    <button onClick={() => handleClick(mesaj)}
+    <button onClick={handleClick}
       style={{ border: `1px solid ${color}`, color: `${color}` }}>{text}</button>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from './Button'
 
-export default function Main() {
+export default function Main({ text, color }) {
+  const [textButton, setTextButton] = useState('Apasa-l')
+
+  function handleButtonClick(textButon) {
+    setTextButton(textButon)
+  }
+
   return (
-    <h1>Welcome to Boom Town!</h1>
+    <>
+      <h1>Welcome to Boom Town!</h1>
+      <Button handleClick={() => handleButtonClick(text)} text={textButton}
+        color={color}
+      />
+    </>
   )
 }
