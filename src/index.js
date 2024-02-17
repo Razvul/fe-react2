4078e73544dbb6b74cb45292a1cf4999ff416a5d
Razvan <razvule@gmail.com>
2024-02-17 18:48:36 +0200

    Am schimbat titlul si am scos Button2

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Main from './components/Main';
 import Button from './components/Button';
-import Button2 from './components/Button2';
-import CountrySelector from './components/CountrySelector';
-import Register from './components/Register';
+import Button2 from './components/Button2'
 
 const mesaj1 = 'Ai apasat cumva pe buton?'
 const textButon1 = 'Apasa-l'
@@ -13,8 +11,9 @@ const textButon1 = 'Apasa-l'
 const mesaj2 = 'Cum ai ajuns aici?'
 const textButon2 = 'BOOM'
 
-const mesaj3='Bine ai venit in alerta!'
-const textButon3='Push'
+const mesaj3 = 'Bine ai venit in alerta!'
+const textButon3 = 'Push'
+
 
 function apasaButon(mesaj) {
   return alert(mesaj)
@@ -24,12 +23,9 @@ function apasaButon(mesaj) {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Main />
-    <Button mesaj={mesaj1} text={textButon1} handleClick={apasaButon} color='green'/>
-    <Button mesaj={mesaj2} text={textButon2} handleClick={apasaButon} color='red' />
-    <Button mesaj={mesaj3} text={textButon3} handleClick={apasaButon} color='blue'/>
-    <Button2 text={textButon2} color='red'/>
-    <CountrySelector />
-    {/* <Register /> */}
+    <Main text={textButon2} color='orange' />
+    <Button mesaj={mesaj1} text={textButon1} handleClick={() => apasaButon(mesaj1)} color='green' />
+    <Button mesaj={mesaj2} text={textButon2} handleClick={() => apasaButon(mesaj2)} color='red' />
+    <Button mesaj={mesaj3} text={textButon3} handleClick={() => apasaButon(mesaj3)} color='blue' />
   </React.StrictMode>
 )
