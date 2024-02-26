c258b6fbe49011b7b0cbfe5877c09adbf1307ecc
Razvan <razvule@gmail.com>
2024-02-26 10:20:51 +0200

    Am creat tabla de sah

---
+++ src/components/TablaSah.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './TablaSah.css'
+
+export default function TablaSah() {
+  return (
+    <section>
+        <div className='patrat-sah' style={{backgroundColor:'red'}}></div>
+        <div className='patrat-sah' style={{backgroundColor:'blue'}}></div>
+        <div className='patrat-sah' style={{backgroundColor:'blue'}}></div>
+        <div className='patrat-sah' style={{backgroundColor:'red'}}></div>
+    </section>
+  )
+}
