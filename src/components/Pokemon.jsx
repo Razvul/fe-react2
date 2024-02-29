d6b35769ca75bc6cc1ec82c631d2b5b615c83ed9
Razvan <razvule@gmail.com>
2024-02-29 17:26:59 +0200

    Am modificat index.jsx

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,23 +1,12 @@
-import { Link, Route, Switch } from 'wouter'
-import Bulbasaur from './Bulbasaur'
-import Charmender from './Charmender'
-import Squirtle from './Squirtle'
+
+
 
 export default function Pokemon() {
 
 
     return (
         <div>
-            <br /> Aici avem un pokemon <br />
-            <Link href='/pokemon/bulbasaur'>Bulbasaur</Link>
-            <Link href='/pokemon/charmender'>Charmender</Link>
-            <Link href='/pokemon/squirtle'>Squirtle</Link>
-
-            <Switch>
-                <Route path='/pokemon/bulbasaur' component={Bulbasaur} > <Bulbasaur/> </Route>
-                <Route path='/pokemon/charmender' component={Charmender} > <Charmender/> </Route>
-                <Route path='/pokemon/squirtle' component={Squirtle} >  </Route>
-            </Switch>
+            Aici avem un pokemon
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Link, Route, Switch } from 'wouter'
+import { Link, Route, Routes, Outlet, BrowserRouter } from 'react-router-dom'
 import Main from './components/Main';
 import TablaSah from './components/TablaSah';
 import Pokemon from './components/Pokemon';
-
+import Bulbasaur from './components/Bulbasaur'
+import Charmender from './components/Charmender'
+import Squirtle from './components/Squirtle'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Link href='/'>Main</Link>
-    <Link href='/tablasah'>Tabla Sah</Link>
-    <Link href='/pokemon'>Pokemon</Link>
+    <BrowserRouter>
+      <Outlet />
+      <nav>
+        <Link href='/'>Main</Link>
+        <Link href='/tablasah'>Tabla Sah</Link>
+        <Link href='/pokemon'>Pokemon</Link>
+      </nav>
 
-    <Switch>
-      <Route path='/' component={Main}></Route>
-      <Route path='/tablasah' component={TablaSah} > </Route>
-      <Route path='/pokemon/:'  component={Pokemon}> </Route>
-    </Switch>
+      <Routes>
+        <Route path='/' element={Main} />
+        <Route path='/tablasah' element={TablaSah} />
+        <Route path='/pokemon' element={Pokemon}>
+          {/* <Outlet />
+        <Link href='/pokemon/bulbasaur'>Bulbasaur</Link>
+        <Link href='/pokemon/charmender'>Charmender</Link>
+        <Link href='/pokemon/squirtle'>Squirtle</Link> */}
+          <Route path='/pokemon/bulbasaur' element={Bulbasaur} >  </Route>
+          <Route path='/pokemon/charmender' element={Charmender} > </Route>
+          <Route path='/pokemon/squirtle' element={Squirtle} >  </Route>
+        </Route>
+      </Routes></BrowserRouter>
   </React.StrictMode>
 )
