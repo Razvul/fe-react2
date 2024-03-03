305df1264f7fa216b9e55484f0be1f86741c021d
Razvan <razvule@gmail.com>
2024-03-03 14:33:42 +0200

    Am facut Promisiuni

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,9 +1,49 @@
+import { useEffect, useRef } from "react"
 import { Link, Outlet } from "react-router-dom"
 
+const url = 'https://unpkg.com/pokeapi-js-wrapper/dist/index.js'
 
 export default function Pokemon() {
 
+    const Pokedex = require("pokeapi-js-wrapper")
+    const P = new Pokedex.Pokedex()
 
+    useEffect(() => {
+        const script = document.createElement("script")
+        script.src = url
+        script.async = true
+        document.body.appendChild(script)
+        // console.log(script)
+
+        // (async () => {
+        //     const golduck = await P.getPokemonByName('golduck')
+        //     console.log(golduck)
+        // })();
+        return () => {
+            document.body.removeChild(script)
+        }
+    }, [])
+
+
+    P.getPokemonByName('charizard').then(function (response) {
+        // response.JSON()
+        pokemonNume = response.name
+        console.log(response)
+    })
+
+    const interval = {
+        offset: 0,
+        limit: 9,
+    }
+
+    P.getPokemonsList(interval).then(function (response) {
+        console.log(response)
+    })
+
+
+    let pokemonNume = ''
+
+    console.log(pokemonNume)
     return (
         <div>
             Aici avem un pokemon
@@ -12,6 +52,7 @@ export default function Pokemon() {
                 <Link to='charmender'>Charmender</Link>
                 <Link to='squirtle'>Squirtle</Link>
                 <Outlet />
+                <h1>{pokemonNume}</h1>
             </nav>
         </div>
     )
