2811548008b324858741e8fabbf66d1691b34e92
Razvan <razvule@gmail.com>
2024-03-07 17:12:43 +0200

    am facut tabla mai mare

--- src/components/TablaSah.jsx
+++ src/components/TablaSah.jsx
@@ -4,25 +4,77 @@ import './TablaSah.css'
 export default function TablaSah() {
   return (
     <section>
-        <div className='patrat-sah' style={{backgroundColor:'red'}}></div>
-        <div className='patrat-sah' style={{backgroundColor:'blue'}}></div>
-        <div className='patrat-sah' style={{backgroundColor:'red'}}></div>
-        <div className='patrat-sah' style={{backgroundColor:'blue'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
 
-        <div className='patrat-sah' style={{backgroundColor:'blue'}}></div>
-        <div className='patrat-sah' style={{backgroundColor:'red'}}></div>
-        <div className='patrat-sah' style={{backgroundColor:'blue'}}></div>
-        <div className='patrat-sah' style={{backgroundColor:'red'}}></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
 
-        <div className='patrat-sah' style={{backgroundColor:'red'}}></div>
-        <div className='patrat-sah' style={{backgroundColor:'blue'}}></div>
-        <div className='patrat-sah' style={{backgroundColor:'red'}}></div>
-        <div className='patrat-sah' style={{backgroundColor:'blue'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
 
-        <div className='patrat-sah' style={{backgroundColor:'blue'}}></div>
-        <div className='patrat-sah' style={{backgroundColor:'red'}}></div>
-        <div className='patrat-sah' style={{backgroundColor:'blue'}}></div>
-        <div className='patrat-sah' style={{backgroundColor:'red'}}></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
+        <div className='patrat-sah' style={{backgroundColor:'black'}}></div>
+        <div className='patrat-sah' ></div>
     </section>
   )
 }
