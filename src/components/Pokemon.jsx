7cd695620926e29a56f83b04c32bba3a3e7204b7
Razvan <razvule@gmail.com>
2024-03-07 22:20:10 +0200

    au aparut pokemonii

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react"
 import { Link, Outlet } from "react-router-dom"
 
-
 const Pokedex = require("pokeapi-js-wrapper")
 const P = new Pokedex.Pokedex()
 
@@ -9,41 +8,40 @@ export default function Pokemon() {
 
 
     const [listaPokemoni, setListaPokemoni] = useState('')
-    // const [listaPokemoniImagine, setListaPokemoniImagine] = useState('')
+    const [listaImagini, setListaImagini] = useState('')
 
     const interval = {
         offset: 0,
         limit: 12,
-    }
+    };
 
     useEffect(() => {
-        P.getPokemonsList(interval).then(function (response) {
-            console.log(response)
-            setListaPokemoni(response)
-            console.log(listaPokemoni)
-        })
+        P.getPokemonsList(interval).then(async (response) => {
 
-        // P.getPokemonFormByName(interval).then(function (response){
-        //     console.log(response)
-        //     setListaPokemoni(response)
-        //     console.log(listaPokemoni)
-        // })
+            setListaPokemoni(response)
+            console.log(response)
+
+            response.results.map((pokemon, i) => {
+                P.getPokemonByName(pokemon.name).then(
+                    async (raspuns) => {
+                        response.results[i].details = raspuns
+                        setListaPokemoni(response)
+                        console.log(response)
+                    }
+                )
+            })
+        })
     }, [])
+    console.log(listaPokemoni)
 
     return (
         <div>
             Aici avem un pokemon
-            {/* <nav>
-                <Link to='bulbasaur'>Bulbasaur</Link>
-                <Link to='charmender'>Charmender</Link>
-                <Link to='squirtle'>Squirtle</Link>
-                <Outlet />
-            </nav> */}
             <div style={{ display: "flex", justifyContent: 'space-around' }}>
                 {listaPokemoni && listaPokemoni.results.map(pokemon => (
-                    <div key={pokemon.id}>
+                    <div key={pokemon.name}>
                         <p>{pokemon.name}</p>
-                        <img src={pokemon.url} alt={pokemon.nume} />
+                        <img src={pokemon.details && pokemon.details.sprites['front_default']} alt={pokemon.nume} />
                     </div>
                 ))}
             </div>
