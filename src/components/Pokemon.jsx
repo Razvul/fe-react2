98062195fdb4a7ae1f4237228b576d80b1e2c114
Razvan <razvule@gmail.com>
2024-03-08 19:34:26 +0200

    am creat componenta Pokemon

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,50 +1,16 @@
-import { useEffect, useState } from "react"
-import { Link, Outlet } from "react-router-dom"
+import React from 'react'
+import { useLocation } from "react-router-dom"
+import './Pokemon.css'
 
-const Pokedex = require("pokeapi-js-wrapper")
-const P = new Pokedex.Pokedex()
-
-export default function Pokemon() {
-
-
-    const [listaPokemoni, setListaPokemoni] = useState('')
-    const [listaImagini, setListaImagini] = useState('')
-
-    const interval = {
-        offset: 0,
-        limit: 12,
-    };
-
-    useEffect(() => {
-        P.getPokemonsList(interval).then(async (response) => {
-
-            setListaPokemoni(response)
-            console.log(response)
-
-            response.results.map((pokemon, i) => {
-                P.getPokemonByName(pokemon.name).then(
-                    async (raspuns) => {
-                        response.results[i].details = raspuns
-                        setListaPokemoni(response)
-                        console.log(response)
-                    }
-                )
-            })
-        })
-    }, [])
-    console.log(listaPokemoni)
-
+export default function Pokemon({ nume, imagine, tip, handleClick }) {
+    const location = useLocation()
+    
     return (
-        <div>
-            Aici avem un pokemon
-            <div style={{ display: "flex", justifyContent: 'space-around' }}>
-                {listaPokemoni && listaPokemoni.results.map(pokemon => (
-                    <div key={pokemon.name}>
-                        <p>{pokemon.name}</p>
-                        <img src={pokemon.details && pokemon.details.sprites['front_default']} alt={pokemon.nume} />
-                    </div>
-                ))}
-            </div>
+        <div className='pokemon' onClick={handleClick}>
+            <h2>{nume}</h2>
+            {/* <h2>{location}</h2> */}
+            <img src={imagine} alt={imagine} />
+            <p>{tip}</p>
         </div>
     )
 }
